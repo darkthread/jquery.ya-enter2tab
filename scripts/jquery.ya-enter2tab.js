f9c44d5816fb05603be7a8b5bbcfa16aaf37a447
darkthread <coding.mz@gmail.com>
2014-04-28 21:46:10 +0800

    Focus loop grouping refactoring

--- scripts/jquery.ya-enter2tab.js
+++ scripts/jquery.ya-enter2tab.js
@@ -1,6 +1,6 @@
 ﻿/*!
  * jQuery Yet Another Enter To Tab plugin
- * Version 0.9
+ * Version 0.9.5
  * @requires jQuery v1.9 or later
  *
  * by Jeffrey Lee, http://blog.darkthread.net/
@@ -9,69 +9,72 @@
  * http://www.gnu.org/licenses/gpl.html
  *
  */
+/*
+
+ver 0.9 2014-04-26 
+ * initial version
+ver 0.9.5 2014-04-28
+ * focus loop grouping logic refactored
+ * add options to enableEnterToTab()
+ * add "captureTabKey" option to capture tab keydown event, prevent focus from going out of container
+*/
+ 
 (function () {
-	function focusPrev(pool) {
-		var a = document.activeElement;
-		if (!a || pool.index(a) == -1) {
-			pool.first().focus();
-			return;
-		}
-		var currIdx = pool.index(a);
-		if (currIdx > 0) pool.eq(currIdx - 1).focus();
-		else pool.last().focus();
-	};
-	function focusNext(pool) {
-		var a = document.activeElement;
-		if (!a || pool.index(a) == -1) {
-			pool.first().focus();
-			return;
-		}
-		var currIdx = pool.index(a);
-		if (currIdx < pool.length - 1) pool.eq(currIdx + 1).focus();
-		else pool.first().focus();
-	};
-	var INPUT_SELECTOR = "input,select,textarea,button", IGNORE_CSS = "e2t-ignore";
-	jQuery.fn.getFocusCandidates = function () {
-		return this.find(INPUT_SELECTOR).filter("[tabindex]:not([tabindex=-1])")
+    function focusPrev(pool) {
+        var a = document.activeElement;
+        if (!a || pool.index(a) == -1) {
+            pool.first().focus();
+            return;
+        }
+        var currIdx = pool.index(a);
+        if (currIdx > 0) pool.eq(currIdx - 1).focus();
+        else pool.last().focus();
+    };
+    function focusNext(pool) {
+        var a = document.activeElement;
+        if (!a || pool.index(a) == -1) {
+            pool.first().focus();
+            return;
+        }
+        var currIdx = pool.index(a);
+        if (currIdx < pool.length - 1) pool.eq(currIdx + 1).focus();
+        else pool.first().focus();
+    };
+    var INPUT_SELECTOR = "input,select,textarea,button", IGNORE_CSS = "e2t-ignore";
+    jQuery.fn.getFocusCandidates = function (tabRange) {
+        return this.find(INPUT_SELECTOR).filter("[tabindex]:not([tabindex=-1])")
 			//ignore disabled, readonly or invisible inputs
-			.filter(":not([disabled]):not([readonly]):visible")
-            //ignore input not joining enter to tab
-            .filter(":not(." + IGNORE_CSS + ")")
+			.filter(
+                ":not([disabled]):not([readonly]):visible" +
+                //for enter key, e2t-ignore fields are excluded
+                //for tab key, they are included
+                (tabRange ? "" : ":not(." + IGNORE_CSS + ")")
+            )
 			//order by tabindex
 			.sort(function (a, b) { return a.tabIndex > b.tabIndex ? 1 : -1; });
-	};
-	function hideTabIndex($container) {
-		$container.find(INPUT_SELECTOR).filter("[tabindex]").each(function () {
-			var $elem = $(this), tabIdx = $elem.attr("tabindex");
-			if (tabIdx == "-1") return;
-			$elem.attr("data-tab-index", tabIdx).attr("tabindex", -1);
-		});
-	};
-	function restoreTabIndex($container) {
-		$container.find(INPUT_SELECTOR).filter("[data-tab-index]").each(function () {
-			var $elem = $(this), storedTabIndex = $elem.attr("data-tab-index");
-			$elem.attr("tabindex", storedTabIndex).removeAttr("data-tab-index");
-		});
-	};
-
-	var CONTAINER_CSS = "e2t-container", ACTIVE_CSS = "e2t-active";
-	jQuery.fn.enableEnterToTab = function () {
-		return this.each(function () {
-			var $container = $(this);
-			$container.addClass(CONTAINER_CSS)
-            .on("keydown", "[tabindex]:not(textarea,.e2t-ignore,.e2t-keyoff)", function (e) {
-            	if (e.which == 13) {
-            		e.preventDefault();
-            		focusNext($container.getFocusCandidates());
-            	}
-            })
-            .on("click", function () {
-            	var $container = $(this).closest("." + CONTAINER_CSS);
-            	if ($container.hasClass(ACTIVE_CSS)) return;
-            	hideTabIndex($("." + CONTAINER_CSS).not(this).removeClass(ACTIVE_CSS).filter(":visible"));
-            	restoreTabIndex($container.addClass(ACTIVE_CSS));
+    };
+    var CONTAINER_CSS = "e2t-container", ACTIVE_CSS = "e2t-active";
+    jQuery.fn.enableEnterToTab = function (options) {
+        var settings = $.extend({
+            //let ya-enter2tab control tab key behavior, prevent focus going outside of the container
+            captureTabKey: false 
+        }, options);
+        return this.each(function () {
+            var $container = $(this);
+            $container.addClass(CONTAINER_CSS)
+            .on("keydown", "[tabindex]:not(textarea)", function (e) {
+                var isTab = e.which == 9;
+                var isEnter = e.which == 13;
+                var $fld = $(this);
+                var isIgnore = $fld.is(".e2t-ignore"), isKeyOff = $fld.is(".e2t-keyoff");
+                if (isEnter && (isIgnore || isKeyOff))
+                    return;
+                if (isEnter || (settings.captureTabKey && isTab)) {
+                    e.preventDefault();
+                    focusNext($container.getFocusCandidates(isTab));
+                }
             });
-		});
-	};
+        });
+    };
 
 })(jQuery);
